fd901ac0f8640914c1850a2d9efafefeeb993ac2
𝐋𝐚𝐛𝐢𝐛 𝐕𝐚𝐢𝐲𝐚✞☠︎ 🚩 <shahriarahmanlabib@gmail.com>
2026-06-10 05:38:20 +0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Labib Md',
+  author: '‎',
+  botName: "labib",
+  botOwner: 'labib vaiya', // Your name
+  ownerNumber: '88017120113', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "0.1.2",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
